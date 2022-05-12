95e65343150c68f965b70fb533b97a6578429743
Alessio Roberto <darthpelo@gmail.com>
2022-05-12 11:58:32 +0200

    Create PlaceholderView and start develop main screen

---
+++ RijksMuseum/RijksMuseum/Screens/Collection/View/CollectionPlaceholderView.swift
@@ -0,0 +1,73 @@
+//
+//  CollectionPlaceholderView.swift
+//  RijksMuseum
+//
+//  Created by Alessio Roberto on 12/05/22.
+//
+
+import SnapKit
+import UIKit
+
+/// Collection placeholder
+final class CollectionPlaceholderView: UIView {
+    private let stackView = UIStackView()
+    private let imageView = UIImageView()
+    private let titleLabel = UILabel()
+
+    private let appearance = Appearance()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupSubviews()
+        setupConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    /// Assigns a displaying image
+    /// - Parameter image: Image model
+    func setImage(_ image: UIImage) {
+        imageView.image = image
+    }
+
+    /// Assigns a title label text
+    /// - Parameter title: Text
+    func setTitle(_ title: String) {
+        titleLabel.text = title
+    }
+
+    private func setupSubviews() {
+        addSubview(stackView)
+        stackView.axis = appearance.stackViewAxis
+        stackView.alignment = appearance.stackViewAlignment
+        stackView.spacing = appearance.stackViewSpacing
+
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(titleLabel)
+
+        titleLabel.numberOfLines = appearance.titleLabelNumberOfLines
+    }
+
+    private func setupConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.top.left.greaterThanOrEqualToSuperview()
+            make.bottom.right.lessThanOrEqualToSuperview()
+        }
+    }
+}
+
+// MARK: - Appearance
+
+private extension CollectionPlaceholderView {
+    struct Appearance {
+        let stackViewAxis: NSLayoutConstraint.Axis = .vertical
+        let stackViewAlignment: UIStackView.Alignment = .center
+        let stackViewSpacing: CGFloat = 5.0
+        let titleLabelNumberOfLines: Int = 0
+    }
+}
--- RijksMuseum/RijksMuseum/Screens/Collection/View/CollectionViewController.swift
+++ RijksMuseum/RijksMuseum/Screens/Collection/View/CollectionViewController.swift
@@ -5,11 +5,17 @@
 //  Created by Alessio Roberto on 12/05/22.
 //
 
+import SnapKit
 import UIKit
 
 /// Displays the search results screen
 final class CollectionViewController: UIViewController {
+    private let appearance = Appearance()
     private let viewModel: CollectionViewModel
+
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
+    private let placeholderView = CollectionPlaceholderView()
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
     /// Designated initializer
     /// - Parameter viewModel: ViewModel
@@ -21,5 +27,73 @@ final class CollectionViewController: UIViewController {
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupObservers()
+        setupSubviews()
+        setupConstraints()
+
+        viewModel.onViewLoad()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if !placeholderView.isHidden {
+            placeholderView.frame = .init(
+                x: 0.0,
+                y: 0.0,
+                width: collectionView.frame.width,
+                height: view.safeAreaLayoutGuide.layoutFrame.maxY
+            )
+
+            placeholderView.bounds = placeholderView.frame
+        }
+    }
+
+    private func setupObservers() {}
+    private func setupSubviews() {
+        navigationController?.isNavigationBarHidden = appearance.isNavigationBarHidden
+        view.backgroundColor = appearance.backgroundColor
+
+        view.addSubview(collectionView)
+        collectionView.alwaysBounceVertical = true
+        collectionView.showsVerticalScrollIndicator = appearance.collectionViewShowsVerticalScrollIndicator
+
+        collectionView.addSubview(placeholderView)
+        placeholderView.setImage(appearance.placeholderViewImage)
+        placeholderView.setTitle(viewModel.placeholderTitle)
+        placeholderView.isHidden = viewModel.isPlaceholderHidden
+
+        view.addSubview(activityIndicator)
+        activityIndicator.backgroundColor = appearance.activityIndicatorBackgroundColor
+        activityIndicator.layer.cornerRadius = appearance.activityIndicatorBackgroundCornerRadius
+    }
+
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalTo(view)
+        }
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
+    }
+}
+
+// MARK: - Appearance
+
+private extension CollectionViewController {
+    struct Appearance {
+        let isNavigationBarHidden: Bool = true
+        let backgroundColor: UIColor = .systemBackground
+        let searchBarStyle: UISearchBar.Style = .minimal
+        let searchBarTextFieldClearButtonMode: UITextField.ViewMode = .never
+        let collectionViewShowsVerticalScrollIndicator: Bool = false
+        let placeholderViewImage: UIImage = .init(named: "no_results_placeholder") ?? .init()
+        let activityIndicatorBackgroundColor: UIColor = .white
+        let activityIndicatorBackgroundCornerRadius: CGFloat = 2.0
     }
 }
