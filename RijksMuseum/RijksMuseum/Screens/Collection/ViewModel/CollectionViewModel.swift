c83aee7c089d0875a51c92e3603e031c43390080
Alessio Roberto <darthpelo@gmail.com>
2022-05-12 11:12:49 +0200

    Refactor CollectionViewModelTests

--- RijksMuseum/RijksMuseum/Screens/Collection/ViewModel/CollectionViewModel.swift
+++ RijksMuseum/RijksMuseum/Screens/Collection/ViewModel/CollectionViewModel.swift
@@ -15,6 +15,15 @@ final class CollectionViewModel {
     /// Collection list placeholder title label text
     let placeholderTitle: String = "No results"
 
+    /// Pull-to-refresh indicator deactivation event handler
+    var onPullToRefreshDeactivation: (() -> Void)?
+
+    /// Full screen reload event handler
+    var onReload: (() -> Void)?
+
+    /// New sections insertion event handler
+    var onSectionsInsertion: ((IndexSet) -> Void)?
+
     /// Activity indicator animating state
     @Observable
     private(set) var isActivityIndicatorAnimating: Bool = false
@@ -22,6 +31,34 @@ final class CollectionViewModel {
     /// Collection list placeholder visibility indicator
     @Observable
     private(set) var isPlaceholderHidden: Bool = false
+
+    /// Collection model
+    private(set) var collection: [CollectionModel] = [] {
+        didSet {
+            isPlaceholderHidden = !collection.isEmpty
+        }
+    }
+
+    private var currentPage: Int = 1
+    private var pageSize: Int = 10
+
+    private let imagePool = ImagePool(limit: 100)
+
+    private let imageLoadingQueue: OperationQueue = {
+        let result = OperationQueue()
+        result.underlyingQueue = .global(qos: .default)
+        result.maxConcurrentOperationCount = 20
+
+        return result
+    }()
+
+    private let dataLoadingQueue: OperationQueue = {
+        let result = OperationQueue()
+        result.underlyingQueue = .global(qos: .default)
+        result.maxConcurrentOperationCount = 1
+
+        return result
+    }()
 
     private let service: CollectionRequesting
     private let router: CollectionRouting
@@ -32,5 +69,104 @@ final class CollectionViewModel {
     init(service: CollectionRequesting, router: CollectionRouting) {
         self.service = service
         self.router = router
+    }
+
+    /// Handles view loading completion event
+    func onViewLoad() {
+        loadNextPage()
+    }
+
+    /// Handles user reaching the bottom of the scroll event
+    func onBottomDragging() {
+        loadNextPage()
+    }
+
+    /// Handles user using pull-to-refresh event
+    func onPullToRefresh() {
+        dataLoadingQueue.cancelAllOperations()
+        currentPage = 1
+        loadNextPage()
+    }
+
+    // MARK: - Private
+
+    private func loadNextPage() {
+        guard dataLoadingQueue.operationCount == 0 else { return }
+        onPullToRefreshDeactivation?()
+
+        let operation = SyncOperation { [weak self] in
+            self?.getImages()
+        } completion: { [weak self] result in
+            guard let result = result else { return }
+            self?.handleImages(result)
+        }
+
+        dataLoadingQueue.addOperation(operation)
+    }
+
+    private func getImages() -> Result<[(title: String, url: String)], Error>? {
+        var result: Result<[(title: String, url: String)], Error>?
+
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+
+        service.getImages(ofMaxResults: pageSize, forPage: currentPage) { res in
+            result = res
+            dispatchGroup.leave()
+        }
+
+        dispatchGroup.wait()
+
+        return result
+    }
+
+    private func handleImages(_ result: Result<[(title: String, url: String)], Error>) {
+        if case let .success(data) = result {
+            DispatchQueue.main.async {
+                self.appendNewImages(data)
+            }
+        } else if currentPage == 1 {
+            DispatchQueue.main.async {
+                self.collection = []
+                self.onReload?()
+            }
+        }
+    }
+
+    private func appendNewImages(_ data: [(title: String, url: String)]) {
+        isActivityIndicatorAnimating = false
+        onPullToRefreshDeactivation?()
+
+        let newCollection: [CollectionModel] = data.compactMap {
+            guard let url = URL(string: $0.url) else { return nil }
+
+            let imageLoader = ImageLoader(queue: imageLoadingQueue, url: url, cache: imagePool)
+            let model = CollectionModel(
+                imageLoader: imageLoader,
+                imageTitle: $0.title,
+                loadingTitle: "Loading",
+                failureTitle: "Failed to load image\nPress to retry"
+            )
+
+            return model
+        }
+
+        let firstNewSectionIndex = ((currentPage - 1) * pageSize) / columnsCount
+
+        let newSectionsCount = newCollection.count % columnsCount == 0 ?
+            newCollection.count / columnsCount :
+            newCollection.count / columnsCount + 1
+
+        let lastNewSectionIndex = firstNewSectionIndex + newSectionsCount
+
+        if currentPage == 1 {
+            collection = newCollection
+            onReload?()
+        } else {
+            collection.append(contentsOf: newCollection)
+            onSectionsInsertion?(.init(integersIn: firstNewSectionIndex ..< lastNewSectionIndex))
+        }
+
+        currentPage += 1
     }
 }
--- RijksMuseum/RijksMuseumTests/Screens/Collection/ViewModel/CollectionViewModelTests.swift
+++ RijksMuseum/RijksMuseumTests/Screens/Collection/ViewModel/CollectionViewModelTests.swift
@@ -33,4 +33,134 @@ final class CollectionViewModelTests: XCTestCase {
         XCTAssertFalse(subject.isActivityIndicatorAnimating)
         XCTAssertFalse(subject.isPlaceholderHidden)
     }
+
+    func testEmptyQueryRefresh() throws {
+        // given
+        var deactivated: Bool!
+        subject.onPullToRefreshDeactivation = {
+            deactivated = true
+        }
+
+        // when user tries to refresh a screen with no result
+        subject.onPullToRefresh()
+
+        // it should immediately deactivate the refresh control
+        XCTAssertTrue(deactivated)
+    }
+
+    func testNavigation() {
+        // given
+        var deactivated: Bool!
+        subject.onPullToRefreshDeactivation = {
+            deactivated = true
+        }
+
+        // when the view is loaded
+        subject.onViewLoad()
+
+        // it should
+        // 1. Hide activity indicator
+        XCTAssertFalse(subject.isActivityIndicatorAnimating)
+        // 2. Deactivate the refresh control
+        XCTAssertTrue(deactivated)
+    }
+
+    func testReload() {
+        // given
+        let query = "query"
+        router.showCollectionClosure = { completion in
+            completion(query)
+        }
+
+        let collection: [(title: String, url: String)] = [(title: "abc", url: ""),
+                                                          (title: "abc", url: ""),
+                                                          (title: "abc", url: "")]
+        service.getImagesOfMaxResultsForPageClosure = { _, _, completion in
+            completion(.success(collection))
+        }
+
+        let expectation = expectation(description: "view should reload")
+        var then: (() -> Void)?
+        subject.onReload = {
+            expectation.fulfill()
+            then?()
+        }
+
+        // when the view is loaded
+        subject.onViewLoad()
+
+        wait(for: [expectation], timeout: 5.0)
+
+        // then it should request the first page
+        then = { [weak subject] in
+            XCTAssert(subject?.collection.count == collection.count)
+            XCTAssert(subject?.isActivityIndicatorAnimating == false)
+        }
+    }
+
+    func testPageLoad() {
+        // given
+        let query = "query"
+        router.showCollectionClosure = { completion in
+            completion(query)
+        }
+
+        let collection: [(title: String, url: String)] = [(title: "abc", url: ""),
+                                                          (title: "abc", url: ""),
+                                                          (title: "abc", url: "")]
+        service.getImagesOfMaxResultsForPageClosure = { _, _, completion in
+            completion(.success(collection))
+        }
+
+        subject.onReload = { [weak subject] in
+            subject?.onBottomDragging()
+        }
+
+        let expectation = expectation(description: "view should load second page")
+        var insertedSections: IndexSet!
+        var then: (() -> Void)?
+        subject.onSectionsInsertion = {
+            insertedSections = $0
+            expectation.fulfill()
+            then?()
+        }
+
+        // when the view is loaded
+        subject.onViewLoad()
+
+        wait(for: [expectation], timeout: 5.0)
+
+        // then it should request the second page
+        then = { [weak subject] in
+            XCTAssert(subject?.collection.count == collection.count * 2)
+            XCTAssert(insertedSections == .init(integersIn: collection.count ..< collection.count * 2))
+            XCTAssert(subject?.isActivityIndicatorAnimating == false)
+        }
+    }
+
+    func testPlaceholderVisibility() {
+        // given
+        let query = "query"
+        router.showCollectionClosure = { completion in
+            completion(query)
+        }
+
+        let expectation = expectation(description: "service should fail")
+        var then: (() -> Void)?
+        service.getImagesOfMaxResultsForPageClosure = { _, _, completion in
+            completion(.failure(TestError()))
+            expectation.fulfill()
+            then?()
+        }
+
+        // when the view is loaded
+        subject.onViewLoad()
+
+        wait(for: [expectation], timeout: 5.0)
+
+        // then it show the placeholder
+        then = { [weak subject] in
+            XCTAssert(subject?.isPlaceholderHidden == false)
+        }
+    }
 }
